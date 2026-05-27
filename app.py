213aa9874c125eacae94bd1f3213d4583393f805
solswiss <sunstingeer@gmail.com>
2026-05-27 13:47:32 -0500

    add map

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
+from typing import Any, cast
+import warnings
+from typing import Any, cast
+import warnings
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import time
+import friendlywords as fw
+from geopy.geocoders import ArcGIS
+import geopy.distance
+from streamlit.delta_generator import DeltaGenerator
+
+
+fw.preload() # type: ignore
+# mostly to ignore empty display warnings
+warnings.filterwarnings("ignore") # type: ignore
+geolocator = ArcGIS()
+
 
 # drone values
 ALT_DEFAULT = 150.0
@@ -17,86 +30,168 @@ BAT_DRAIN_MIN = .1
 BAT_DRAIN_MAX = .3
 BAT_DRAIN_CEIL = 2
 
-# program values
-SLEEP_DURATION = 1
+# sim values
+DEFAULT_START_LOC = geolocator.geocode('175 5th Avenue NYC',timeout=10) # type: ignore
+DELTA = 1 # 1 time per sec
 
 
 # init data storage
-COLS = ['Time', 'Altitude', 'Battery', 'Motor_Temperature']
+COLS = ['Time', 'Altitude', 'Battery', 'Motor Temperature','Longitude','Latitude']
 def empty_session():
+    if 'start_loc' in st.session_state:
+        del st.session_state.start_loc
+        del st.session_state.start_x
+        del st.session_state.start_y
+        #del st.session_state.session_name
+    # if 'connection_toggle' in st.session_state:
+    #     st.session_state.connection_toggle = False
+    st.session_state.departed = False
+    st.session_state.start_loc = DEFAULT_START_LOC.address # type: ignore
+    st.session_state.start_x = DEFAULT_START_LOC.latitude # type: ignore
+    st.session_state.start_y = DEFAULT_START_LOC.longitude # type: ignore
     st.session_state.flight_history = pd.DataFrame(columns=COLS)
+    st.session_state.lat = st.session_state.start_x
+    st.session_state.lon = st.session_state.start_y
     st.session_state.current_time = 0
     st.session_state.current_battery = BATTERY_MAX
     st.session_state.alerts = []
+    st.session_state.session_name = "mock telemetry "+fw.generate(3) # type: ignore
+    st.session_state.session_name = "mock telemetry "+fw.generate(3) # type: ignore
 
 if "flight_history" not in st.session_state:
     empty_session()
 
 
 # base architecture
-st.title('Drone Dash')
+st.title('Mock Telemetry Dashboard')
 sidebar = st.sidebar
-sidebar.header('Hardware')
 
-run_sim = sidebar.toggle('Connect Telemetry Stream',value=False)
+def connection_changed():
+    if not st.session_state.departed and st.session_state.connection_toggle:
+        st.session_state.departed = True
+run_sim = sidebar.toggle('Connect Telemetry Stream',value=False,key='connection_toggle',on_change=connection_changed)
+
+session_string = sidebar.text_input('Session Name', value=st.session_state.session_name)
 
 sidebar.subheader('Settings')
+
+# physics
+heading = sidebar.slider("heading (deg)",min_value=0,max_value=360,value=45)
+speed = sidebar.slider("speed (m/s)",min_value=1,max_value=30,value=10)
+
+# coords
+def update_start_loc():
+    loc = geolocator.reverse((str(st.session_state.start_x),str(st.session_state.start_y)),timeout=10) #type: ignore
+    if loc:
+        st.session_state.start_loc = loc.address # type: ignore
+        st.session_state.lat = st.session_state.start_x # type: ignore
+        st.session_state.lon = st.session_state.start_y # type: ignore
+        populate_map()
+def update_start_coords():
+    loc = geolocator.geocode(st.session_state.start_loc,timeout=10) #type: ignore
+    if loc:
+        st.session_state.start_x = st.session_state.lat = loc.latitude # type: ignore
+        st.session_state.start_y = st.session_state.lon = loc.longitude # type: ignore
+        populate_map()
+start_coord_loc = sidebar.text_input('address',key='start_loc',on_change=update_start_coords,disabled=st.session_state.departed) # type: ignore
+start_coord_col1, start_coord_col2 = sidebar.columns(2)
+start_x = start_coord_col1.number_input('longitude',key='start_x',on_change=update_start_loc,disabled=st.session_state.departed) # type: ignore
+start_y = start_coord_col2.number_input('latitude',key='start_y',on_change=update_start_loc,disabled=st.session_state.departed) # type: ignore
+
+# faults
+sidebar.subheader('Fault Injections')
 inject_thermal_fault = sidebar.checkbox('overheat motor',value=False)
 inject_alt_tampering = sidebar.checkbox('spoofing attack',value=False)
 inject_bat_tampering = sidebar.checkbox('DOS',value=False)
 
-clear_data = sidebar.button('Reset')
+# data
+download_data = sidebar.download_button('Download Flight History', st.session_state.flight_history.to_csv(index=False), file_name=f'{st.session_state.session_name} flight history.csv')
+download_alerts = sidebar.download_button('Download Alert Log', st.session_state.flight_history.to_csv(index=False), file_name=f'{st.session_state.session_name} alert log.csv')
+clear_data = sidebar.button('Reset',key='clear_data',on_click=empty_session)
 
-status = st.empty()
+# visuals
+sys_banner = st.empty()
+status: DeltaGenerator = st.empty()
 
-if clear_data:
-    empty_session()
-    st.rerun()
+st.markdown('## Metrics')
+col1, col2, col3 = st.columns(3)
+col_altitude = col1.empty()
+col_battery = col2.empty()
+col_motor_temp = col3.empty()
+
+st.markdown('### Live Telemetry Stream')
+tracks = st.multiselect('Tracked data', COLS[1:], default=['Altitude','Motor Temperature'])
+chart_display = st.empty()
+
+st.markdown('## Location')
+map_display = st.empty()
+
+st.markdown('## Raw Data')
+raw_data = st.empty()
+
+st.subheader('Alert Log')
+alert_log = st.empty()
 
 
 def populate_metrics(history_df, anomalies):
     if not history_df.empty:
         latest_row = history_df.iloc[-1] # get most recent read
     else:
-        latest_row = latest_row = pd.Series([0,0.0,BATTERY_MAX,0.0], index=COLS)
-    col1, col2, col3 = st.columns(3)
-    col1.metric('Altitude', f"{latest_row['Altitude']} ft")
-    col2.metric('Battery', f"{latest_row['Battery']}%")
+        latest_row = latest_row = pd.Series([0,0.0,BATTERY_MAX,0.0,st.session_state.lon,st.session_state.lat], index=COLS)
+    col_altitude.metric('Altitude', f"{latest_row['Altitude']} ft")
+    col_battery.metric('Battery', f"{latest_row['Battery']}%")
     if 'overheat' in anomalies:
-        col3.metric('Motor Temperature', f"{latest_row['Motor_Temperature']} °F", delta='Critical overheat', delta_color='inverse')
+        col_motor_temp.metric('Motor Temperature', f"{latest_row['Motor Temperature']} °F", delta='Critical overheat', delta_color='inverse')
+        col_motor_temp.metric('Motor Temperature', f"{latest_row['Motor Temperature']} °F", delta='Critical overheat', delta_color='inverse')
     else:
-        col3.metric('Motor Temperature', f"{latest_row['Motor_Temperature']} °F")
+        col_motor_temp.metric('Motor Temperature', f"{latest_row['Motor Temperature']} °F")
+        col_motor_temp.metric('Motor Temperature', f"{latest_row['Motor Temperature']} °F")
 
 def populate_chart(history_df, anomalies):
-    st.header('Live Telemetry Stream')
+    chart_colors = ['#0068c9','#83c9ff','#29b09d']
     data = history_df.tail(30)
-    if 'overheat' in anomalies or (temp > TEMP_CEILING for temp in data['Motor_Temperature']):
-        mask = data['Motor_Temperature'] > TEMP_CEILING
-        data['Overheated'] = data[mask]['Motor_Temperature']
-        st.line_chart(data, x='Time', y=['Altitude','Motor_Temperature','Overheated'], color=['#0068c9','#83c9ff','red'])
+    if 'overheat' in anomalies or (temp > TEMP_CEILING for temp in data['Motor Temperature']):
+        mask = data['Motor Temperature'] > TEMP_CEILING
+        data['Overheated'] = data[mask]['Motor Temperature']
+        chart_display.line_chart(data, x='Time', y=tracks+['Overheated'], color=chart_colors[:len(tracks)]+['red']) # type: ignore
+        chart_display.line_chart(data, x='Time', y=tracks+['Overheated'], color=chart_colors[:len(tracks)]+['red']) # type: ignore
     else:
-        st.line_chart(data, x='Time', y=['Altitude','Motor_Temperature'])
+        chart_display.line_chart(data, x='Time', y=tracks, color=chart_colors[:len(tracks)]) # type: ignore
+
+def populate_map():
+    map_display.map(pd.DataFrame({'lat': [st.session_state.lat], 'lon': [st.session_state.lon]}))
 
 def populate_log(history_df, anomalies):
-    st.header('Raw Data')
-    st.dataframe(history_df.tail(10), width='stretch')
-    st.subheader('Alert Log')
-    st.dataframe(pd.DataFrame(st.session_state.alerts).tail(5))
+    raw_data.dataframe(history_df.tail(10), width='stretch')
+    alert_log.dataframe(pd.DataFrame(st.session_state.alerts).tail(5))
 
-@st.fragment(run_every=SLEEP_DURATION)
+# drone physics
+def move_drone(lat, lon, bearing_deg, speed_mps, delta):
+    dist_m = speed_mps*delta
+    start_coords = (lat,lon)
+    moving_dist = geopy.distance.distance(meters=dist_m)
+    dest = moving_dist.destination(point=start_coords,bearing=bearing_deg)
+    return dest.latitude, dest.longitude
+
+
+@st.fragment(run_every=DELTA)
 def drone_dashboard():
     if st.session_state.current_battery == 0:
-        st.info('System offline. Restart the connection in the sidebar to stream telemetry live.')
+        sys_banner.info('System offline. Restart the connection in the sidebar to stream telemetry live.')
     elif not (run_sim and st.session_state.current_battery > 0):
-        st.info('System offline. Toggle connection in the sidebar to stream telemetry live.')
-        st.session_state.flight_history.to_csv('data.csv',index=False) # store data
+        sys_banner.info('System offline. Toggle connection in the sidebar to stream telemetry live.')
     else:
-        # generate data - constants are listed for convenience
+        # generate data
+        # 'constants' are listed for convenience
         st.session_state.current_time += 1 # clock
         alt_noise = 1.5
         alt_tampering_noise = 100.0
         temp_noise = .8
         temp_thermal_fault_noise = 3.0
+
+        lat, lon = move_drone(st.session_state.lat,st.session_state.lon,heading,speed,DELTA)
+        st.session_state.lat = lat
+        st.session_state.lon = lon
 
         if inject_alt_tampering: alt = np.random.normal(ALT_DEFAULT*2.5,alt_tampering_noise)
         else: alt = np.random.normal(ALT_DEFAULT,alt_noise) # altitude
@@ -108,7 +203,15 @@ def drone_dashboard():
         else: st.session_state.current_battery -= np.random.uniform(BAT_DRAIN_MIN,BAT_DRAIN_MAX) # drain battery
         st.session_state.current_battery = max(0.0, st.session_state.current_battery) # 0 min
 
-        df = pd.DataFrame([[st.session_state.current_time,round(alt,2),round(st.session_state.current_battery,1),round(temp,2)]], columns=COLS)
+        df = pd.DataFrame([[
+            st.session_state.current_time,
+            round(alt,2),
+            round(st.session_state.current_battery,1),
+            round(temp,2),
+            lon,
+            lat,
+            ]],
+            columns=COLS)
 
         st.session_state.flight_history = pd.concat([st.session_state.flight_history,df], ignore_index=True)
 
@@ -132,7 +235,7 @@ def drone_dashboard():
                 'Message':f'Warning: battery level is below {BAT_WARN_FLOOR}'
             })
         
-        if latest_row['Motor_Temperature'] > TEMP_CEILING:
+        if latest_row['Motor Temperature'] > TEMP_CEILING:
             anomalies['overheat'] = f'Motor overheating detected at {latest_row['Time']}s: current temperatures exceed safety threshold'
             status.error(anomalies['overheat'])
             st.session_state.alerts.append({
@@ -164,11 +267,12 @@ def drone_dashboard():
                 'Message':anomalies['spoof']
             })
     else:
-        latest_row = pd.Series([0,0.0,BATTERY_MAX,0.0], index=COLS)
+        latest_row = pd.Series([0,0.0,BATTERY_MAX,0.0,st.session_state.lon,st.session_state.lat], index=COLS)
 
     # live render
     populate_metrics(history_df, anomalies)
     populate_chart(history_df, anomalies)
+    populate_map()
     populate_log(history_df, anomalies)
 
 drone_dashboard()
